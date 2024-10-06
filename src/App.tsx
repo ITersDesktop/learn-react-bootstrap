8cb01f8aac8e1e608a36e1fcf5ed1871a8467b49
Tung Nguyen <nvntung@gmail.com>
2024-10-06 09:17:21 +0100

    Update ViteEx comp to use BS classes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import './App.css'
 import NavBar from "./components/Navbar.tsx";
-// import ViteEx from "./components/ViteEx.tsx";
+import ViteEx from "./components/ViteEx.tsx";
 
 function App() {
   return (
     <>
         <NavBar/>
-        {/*<ViteEx/>*/}
+        <ViteEx/>
     </>
   )
 }
