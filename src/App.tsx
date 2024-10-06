ad03bde3096539ca7f2e9ec3fbd7da47ea52168a
Tung Nguyen <nvntung@gmail.com>
2024-10-06 09:11:35 +0100

    Push the first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import NavBar from "./components/Navbar.tsx";
+// import ViteEx from "./components/ViteEx.tsx";
+
+function App() {
+  return (
+    <>
+        <NavBar/>
+        {/*<ViteEx/>*/}
+    </>
+  )
+}
+
+export default App
